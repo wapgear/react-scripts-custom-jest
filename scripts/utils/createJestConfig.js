17b95f81ea6ce0e63e6c3aace31d57458319cba5
Anton Izmailov <wapgear4@gmail.com>
2017-12-08 18:10:11 +0300

    Update createJestConfig.js

--- scripts/utils/createJestConfig.js
+++ scripts/utils/createJestConfig.js
@@ -21,7 +21,7 @@ module.exports = (resolve, rootDir, isEjecting) => {
   // TODO: I don't know if it's safe or not to just use / as path separator
   // in Jest configs. We need help from somebody with Windows to determine this.
   const config = {
-    collectCoverageFrom: ['src/**/*.{js,jsx,mjs}'],
+    collectCoverageFrom: ['src/**/*.{js,jsx,mjs,tsx}'],
     setupFiles: [resolve('config/polyfills.js')],
     setupTestFrameworkScriptFile: setupTestsFile,
     testMatch: [
